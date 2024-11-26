35a9aa1fea763eca60f6dffcf3d3014467df4915
Manuela-PELAGHO-MBOU-3IL <pelaghom@3il.fr>
2024-11-26 14:25:04 +0100

    correction exercice 3 : optimisation des performances de calcul

--- exercice_3/exercice3.java
+++ exercice_3/exercice3.java
@@ -1,22 +1,60 @@
+/**
+ * Classe StockManager : permet de gérer le stock des produits.
+ */
 public class StockManager {
 
-    // gere le stock
-    public void gererStock(String typeOperation, String produit, int quantite, int stock) {
-        // Ajout au stock
-        if (typeOperation.equals("ajout")) {
-            stock += quantite;
-            System.out.println("Produit : " + produit + ", Stock après ajout : " + stock);
-        // Retrait du stock
-        } else if (typeOperation.equals("retrait")) {
-            if (stock >= quantite) {
-                stock -= quantite;
-                System.out.println("Produit : " + produit + ", Stock après retrait : " + stock);
-            } else {
-                System.out.println("Stock insuffisant pour le produit : " + produit);
-            }
-        // Si l'action est inconnu
-        } else {
-            System.out.println("Opération inconnue.");
+    /**
+     * Gère les opérations de stock (ajout ou retrait).
+     *
+     * @param typeOperation Type d'opération ("ajout" ou "retrait")
+     * @param produit        Nom du produit
+     * @param quantite       Quantité à ajouter ou retirer
+     * @param stock          Stock actuel
+     * @return Nouveau stock après l'opération
+     */
+    public int gererStock(String typeOperation, String produit, int quantite, int stock) {
+        switch (typeOperation.toLowerCase()) {
+            case "ajout":
+                return ajouterStock(produit, quantite, stock);
+
+            case "retrait":
+                return retirerStock(produit, quantite, stock);
+
+            default:
+                System.out.println("Opération inconnue pour le produit : " + produit);
+                return stock;
         }
+    }
+
+    /**
+     * Ajoute une quantité au stock.
+     *
+     * @param produit  Nom du produit
+     * @param quantite Quantité à ajouter
+     * @param stock    Stock actuel
+     * @return Nouveau stock après l'ajout
+     */
+    private int ajouterStock(String produit, int quantite, int stock) {
+        stock += quantite;
+        System.out.println("Produit : " + produit + ", Stock après ajout : " + stock);
+        return stock;
+    }
+
+    /**
+     * Retire une quantité du stock si suffisant.
+     *
+     * @param produit  Nom du produit
+     * @param quantite Quantité à retirer
+     * @param stock    Stock actuel
+     * @return Nouveau stock après le retrait
+     */
+    private int retirerStock(String produit, int quantite, int stock) {
+        if (stock >= quantite) {
+            stock -= quantite;
+            System.out.println("Produit : " + produit + ", Stock après retrait : " + stock);
+        } else {
+            System.out.println("Stock insuffisant pour le produit : " + produit);
+        }
+        return stock;
     }
 }
